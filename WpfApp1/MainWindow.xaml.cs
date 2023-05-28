f0e19e0cbf9ee963f222cefc66a219263494b623
Alexey <Alexey@ALEX>
2023-05-28 21:56:31 +0700

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Npgsql;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public void InitializeDataTable()
+        {
+            NpgsqlConnection con = new NpgsqlConnection("Host=localhost;Port=5432;Database=test;Username=postgres;Password=1234");
+            try
+            {
+                con.Open();
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+            catch (System.IO.IOException)
+            {
+                MessageBox.Show("Error");
+                con.Close();
+            }
+            string sql = $"SELECT * FROM test";
+            NpgsqlCommand command = new NpgsqlCommand(sql, con);
+            DataTable dt = new DataTable();
+            dt.Load(command.ExecuteReader(CommandBehavior.CloseConnection));
+            DataGrid1.DataContext = dt;
+            DataGrid1.ItemsSource = dt.AsDataView();
+        }
+        public MainWindow()
+        {
+           InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           InitializeDataTable();
+        }
+        private void click(object sender, RoutedEventArgs e)
+        {
+            NpgsqlConnection con = new NpgsqlConnection("Host=localhost;Port=5432;Database=test;Username=postgres;Password=1234");
+            try
+            {
+                con.Open();
+            }
+            catch (NpgsqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+            catch (System.IO.IOException)
+            {
+                MessageBox.Show("Error");
+                con.Close();
+            }
+            string sql = "INSERT INTO test(name, email, age) VALUES (@name, @email, @age)";
+            NpgsqlCommand command = new NpgsqlCommand(sql, con);
+            command.Parameters.Add(new NpgsqlParameter("@name", textBox_name.Text));
+            command.Parameters.Add(new NpgsqlParameter("@email", textBox_email.Text));
+            string text_age = textBox_age.Text;
+            int age;
+            bool isConvToInt = int.TryParse(textBox_age.Text, out age);
+            command.Parameters.Add(new NpgsqlParameter("@age", age));
+            command.ExecuteNonQuery();
+            InitializeDataTable();
+        }
+    }
+
+    
+
+}
